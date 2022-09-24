717805cfcffe32fcc50b81936fa00c440099572d
ArmeenF <armeenfarange@yahoo.com>
2022-09-24 11:39:46 -0700

    Add Home Page w/Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./Auth"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">React Website Development</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/Auth">Authentication</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </Container>
   )
 }
 
